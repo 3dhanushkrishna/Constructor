cf9601965b6d188968d32e38c99f40be95553fa1
3dhanushkrishna <3dhanushkccd@gmail.com>
2022-03-15 13:53:00 +0530

    constructor

---
+++ src/com/harman/project2/Mainfile.java
@@ -0,0 +1,11 @@
+package com.harman.project2;
+
+public class Mainfile {
+    public static void main(String[] args) {
+        Sample s = new Sample(90);
+        System.out.println(s.x);
+
+        Sample s1 = new Sample(120);
+        System.out.println(s1.x);
+    }
+}
---
+++ src/com/harman/project2/Sample.java
@@ -0,0 +1,10 @@
+package com.harman.project2;
+
+public class Sample {
+    int x;
+
+    public Sample(int x) {
+        this.x = x;
+    }
+}
+
